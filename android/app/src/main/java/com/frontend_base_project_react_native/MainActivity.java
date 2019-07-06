265e420de93d2764b4ded2d4ba4c426fb91da343
Rafael Sene Alves Penha <rafaelssene@gmail.com>
2019-07-05 21:35:34 -0300

    criando novo projeto, iniciando na versão 0.59.10, ja implementando todas modificações anteriores.

--- android/app/src/main/java/com/frontend_base_project_react_native/MainActivity.java
+++ android/app/src/main/java/com/frontend_base_project_react_native/MainActivity.java
@@ -2,6 +2,10 @@ package com.frontend_base_project_react_native;
 
 import com.facebook.react.ReactActivity;
 
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -11,5 +15,15 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "frontend_base_project_react_native";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+          return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
     }
 }
