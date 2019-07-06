7dc0648b8e2664e1217be2b5083b7223d810dd87
Rafael Sene Alves Penha <rafaelssene@gmail.com>
2019-07-05 23:48:51 -0300

    configurando o statusbar.

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,22 @@ import Main from './pages/Main';
 import User from './pages/User';
 
 const Routes = createAppContainer(
-  createStackNavigator({
-    Main,
-    User,
-  })
+  createStackNavigator(
+    {
+      Main,
+      User,
+    },
+    {
+      headerLayoutPreset: 'center',
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+        headerTintColor: '#FFF',
+      },
+    }
+  )
 );
 
 export default Routes;
